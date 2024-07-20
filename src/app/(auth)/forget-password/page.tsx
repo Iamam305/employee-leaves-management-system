e20498624b941145135472deaaea964fc17926ba
Ritik Kumar Pathak <phritik06@gmail.com>
2024-07-20 15:46:35 +0530

    forgot password api

---
+++ src/app/(auth)/forget-password/page.tsx
@@ -0,0 +1,27 @@
+import ChangePassword from "@/components/Auth/ChangePassword";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <div className=" w-full  flex " style={{ height: "calc(100vh)" }}>
+        {/* Left Section */}
+        <div className="md:w-[45%] h-full bg-zinc-900 p-6 relative hidden md:block">
+          <h1 className="text-white font-semibold text-xl ">
+            Employee Management System
+          </h1>
+          <div className=" absolute bottom-10 ">
+            <h1 className="text-white font-semibold text-xl ">
+              Welcome Back!{" "}
+            </h1>
+          </div>
+        </div>
+        <div className="md:w-[55%] w-[100%] h-full  flex items-center justify-center">
+          <ChangePassword />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
---
+++ src/app/api/(auth)/forgot-password/route.ts
@@ -0,0 +1,82 @@
+import { ForgotPasswordEmailTemplate } from "@/components/email-temp/ForgotPasswordTemplate";
+import { User } from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
+import bcrypt from "bcrypt";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req: NextRequest) {
+  try {
+    const { email } = await req.json();
+    const user = await User.findOne({
+      email,
+    });
+    if (!user) {
+      return NextResponse.json(
+        {
+          msg: "user not found",
+        },
+        { status: 401 }
+      );
+    }
+    const data = await resend.emails.send({
+      from: "Acme <team@qtee.ai>",
+      to: email,
+      subject: " Forgot Password",
+      react: ForgotPasswordEmailTemplate({ userId: user?._id.toString() }),
+      html: "5",
+    });
+    return NextResponse.json({
+      msg: "Email Sent Successfully",
+      data,
+    });
+  } catch (error: any) {
+    return NextResponse.json({
+      msg: error.message,
+    });
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  try {
+    const { password, user_id } = await req.json();
+    // const user = await User.findById(user_id);
+
+    const user = await User.findOne({
+      _id: user_id,
+    });
+
+    if (!user) {
+      return NextResponse.json({ msg: "Invalid user" }, { status: 400 });
+    }
+
+    const pepper_password = password + process.env.PEPPER;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(pepper_password, salt);
+
+    await User.updateOne(
+      { _id: user_id },
+      { $set: { password: hashedPassword } },
+      {
+        new: true,
+      }
+    );
+
+    // const user = await User.find
+
+    // user.password = hashedPassword;
+    // await user.save();
+    return NextResponse.json({
+      msg: "Password Changed Successfully",
+      user,
+      hashedPassword,
+      // password
+    });
+  } catch (error: any) {
+    return NextResponse.json({
+      msg: error.message,
+    });
+  }
+}
