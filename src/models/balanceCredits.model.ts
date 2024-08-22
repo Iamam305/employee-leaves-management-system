28b0f8cb6c117497cd789d53474fc32dbb195b23
Sagar-Lekhwani <sgrlekhwani@gmail.com>
2024-08-22 18:08:24 +0530

    neccessary function for balances created

--- src/models/balanceCredits.model.ts
+++ src/models/balanceCredits.model.ts
@@ -1,91 +1,24 @@
 import mongoose from "mongoose";
 
-const balancesSchema = new mongoose.Schema(
+const balanceSchema = new mongoose.Schema(
   {
     year: {
       type: String,
       required: true,
     },
-    annualCredit: {
-      type: Number,
-      default: 0,
-    },
-    annualUsed: {
-      type: Number,
-      default: 0,
-    },
-    annualAvailable: {
-      type: Number,
-      default: 0,
-    },
-    healthCredit: {
-      type: Number,
-      default: 0,
-    },
-    healthUsed: {
-      type: Number,
-      default: 0,
-    },
-    healthAvailable: {
-      type: Number,
-      default: 0,
-    },
-    studyCredit: {
-      type: Number,
-      default: 0,
-    },
-    studyUsed: {
-      type: Number,
-      default: 0,
-    },
-    studyAvailable: {
-      type: Number,
-      default: 0,
-    },
-    maternityCredit: {
-      type: Number,
-      default: 0,
-    },
-    maternityUsed: {
-      type: Number,
-      default: 0,
-    },
-    maternityAvailable: {
-      type: Number,
-      default: 0,
-    },
-    familyCredit: {
-      type: Number,
-      default: 0,
-    },
-    familyUsed: {
-      type: Number,
-      default: 0,
-    },
-    familyAvailable: {
-      type: Number,
-      default: 0,
-    },
-    paternityCredit: {
-      type: Number,
-      default: 0,
-    },
-    paternityUsed: {
-      type: Number,
-      default: 0,
-    },
-    paternityAvailable: {
-      type: Number,
-      default: 0,
-    },
-    unpaidUsed: {
-      type: Number,
-      default: 0,
-    },
     userId: {
       type: mongoose.Types.ObjectId,
       required: true,
       ref: "User",
+    },
+    leaveBalances: {
+      type: Map,
+      of: {
+        credit: { type: Number, default: 0 },
+        used: { type: Number, default: 0 },
+        available: { type: Number, default: 0 },
+      },
+      default: {},
     },
   },
   {
@@ -93,4 +26,4 @@ const balancesSchema = new mongoose.Schema(
   }
 );
 
-export const Balances = mongoose.models.Balances || mongoose.model("Balances", balancesSchema);
+export const Balances = mongoose.model("Balances", balanceSchema);
