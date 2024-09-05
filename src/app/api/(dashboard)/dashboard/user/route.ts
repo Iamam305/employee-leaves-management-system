4c740fcc062771d2566500b27e66855aaa559190
Ritik Kumar Pathak <phritik06@gmail.com>
2024-09-05 11:17:38 +0530

    optimised user-dashboard api

--- src/app/api/(dashboard)/dashboard/user/route.ts
+++ src/app/api/(dashboard)/dashboard/user/route.ts
@@ -26,144 +26,144 @@ export async function GET(req: NextRequest) {
       };
     }
 
-    const accepted_leaves = await Leave.aggregate([
-      {
-        $match: {
-          status: "approved",
-          // org_id: new ObjectId(org_id),
-          user_id: new ObjectId(user_id),
-          ...matchFilter,
-        },
-      },
-      {
-        $group: {
-          _id: {
-            status: "$status",
-            year: { $year: "$createdAt" },
-            month: { $month: "$createdAt" },
-            day: { $dayOfMonth: "$createdAt" },
-          },
-          count: { $sum: 1 },
-        },
-      },
-      {
-        $sort: { "_id.year": 1, "_id.month": 1, "_id.day": 1 },
-      },
-      {
-        $project: {
-          _id: 0,
-          date: {
-            $dateFromParts: {
-              year: "$_id.year",
-              month: "$_id.month",
-              day: "$_id.day",
+    const [accepted_leaves, rejected_leaves, totalLeaves, balances] =
+      await Promise.all([
+        await Leave.aggregate([
+          {
+            $match: {
+              status: "approved",
+              // org_id: new ObjectId(org_id),
+              user_id: new ObjectId(user_id),
+              ...matchFilter,
             },
           },
-          count: 1,
-          firstCreatedAt: 1,
-        },
-      },
-      {
-        $group: {
-          _id: null,
-          totalAcceptedLeaves: { $sum: "$count" },
-          accepted_leaves: { $push: { date: "$date", count: "$count" } },
-        },
-      },
-      {
-        $project: {
-          _id: 0,
-          totalAcceptedLeaves: 1,
-          accepted_leaves: 1,
-        },
-      },
-    ]);
-
-    const rejected_leaves = await Leave.aggregate([
-      {
-        $match: {
-          status: "rejected",
-          // org_id: new ObjectId(org_id),
-          user_id: new ObjectId(user_id),
-          ...matchFilter,
-        },
-      },
-      {
-        $group: {
-          _id: {
-            status: "$status",
-            year: { $year: "$createdAt" },
-            month: { $month: "$createdAt" },
-            day: { $dayOfMonth: "$createdAt" },
-          },
-          count: { $sum: 1 },
-        },
-      },
-      {
-        $sort: { "_id.year": 1, "_id.month": 1, "_id.day": 1 },
-      },
-      {
-        $project: {
-          _id: 0,
-          date: {
-            $dateFromParts: {
-              year: "$_id.year",
-              month: "$_id.month",
-              day: "$_id.day",
+          {
+            $group: {
+              _id: {
+                status: "$status",
+                year: { $year: "$createdAt" },
+                month: { $month: "$createdAt" },
+                day: { $dayOfMonth: "$createdAt" },
+              },
+              count: { $sum: 1 },
             },
           },
-          count: 1,
-        },
-      },
-      {
-        $group: {
-          _id: null,
-          totalRejectedLeaves: { $sum: "$count" },
-          rejecetd_leaves: { $push: { date: "$date", count: "$count" } },
-        },
-      },
-      {
-        $project: {
-          _id: 0,
-          totalRejectedLeaves: 1,
-          rejecetd_leaves: 1,
-        },
-      },
-    ]);
+          {
+            $sort: { "_id.year": 1, "_id.month": 1, "_id.day": 1 },
+          },
+          {
+            $project: {
+              _id: 0,
+              date: {
+                $dateFromParts: {
+                  year: "$_id.year",
+                  month: "$_id.month",
+                  day: "$_id.day",
+                },
+              },
+              count: 1,
+              firstCreatedAt: 1,
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              totalAcceptedLeaves: { $sum: "$count" },
+              accepted_leaves: { $push: { date: "$date", count: "$count" } },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              totalAcceptedLeaves: 1,
+              accepted_leaves: 1,
+            },
+          },
+        ]),
 
-    const totalLeaves = await Leave.aggregate([
-      {
-        $match: {
-          // org_id: new ObjectId(org_id),
-          user_id: new ObjectId(user_id),
-        },
-      },
-      {
-        $group: {
-          _id: "$status",
-          count: { $sum: 1 }, 
-        },
-      },
-      {
-        $group: {
-          _id: null, 
-          leavesByStatus: { $push: { status: "$_id", count: "$count" } }, 
-          totalLeaves: { $sum: "$count" }, 
-        },
-      },
-      {
-        $project: {
-          _id: 0,
-          leavesByStatus: 1,
-          totalLeaves: 1, 
-        },
-      },
-    ]);
+        await Leave.aggregate([
+          {
+            $match: {
+              status: "rejected",
+              // org_id: new ObjectId(org_id),
+              user_id: new ObjectId(user_id),
+              ...matchFilter,
+            },
+          },
+          {
+            $group: {
+              _id: {
+                status: "$status",
+                year: { $year: "$createdAt" },
+                month: { $month: "$createdAt" },
+                day: { $dayOfMonth: "$createdAt" },
+              },
+              count: { $sum: 1 },
+            },
+          },
+          {
+            $sort: { "_id.year": 1, "_id.month": 1, "_id.day": 1 },
+          },
+          {
+            $project: {
+              _id: 0,
+              date: {
+                $dateFromParts: {
+                  year: "$_id.year",
+                  month: "$_id.month",
+                  day: "$_id.day",
+                },
+              },
+              count: 1,
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              totalRejectedLeaves: { $sum: "$count" },
+              rejecetd_leaves: { $push: { date: "$date", count: "$count" } },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              totalRejectedLeaves: 1,
+              rejecetd_leaves: 1,
+            },
+          },
+        ]),
 
-    const balances= await Balances.find(
-      { userId: user_id },
-      { leaveBalances: 1, _id: 0 }
-    );
+        await Leave.aggregate([
+          {
+            $match: {
+              // org_id: new ObjectId(org_id),
+              user_id: new ObjectId(user_id),
+            },
+          },
+          {
+            $group: {
+              _id: "$status",
+              count: { $sum: 1 },
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              leavesByStatus: { $push: { status: "$_id", count: "$count" } },
+              totalLeaves: { $sum: "$count" },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              leavesByStatus: 1,
+              totalLeaves: 1,
+            },
+          },
+        ]),
+
+        await Balances.find({ userId: user_id }, { leaveBalances: 1, _id: 0 }),
+      ]);
 
     return NextResponse.json(
       {
