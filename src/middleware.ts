b7c7ef8af52a91eb4bdb0c520f9aca583b727ed3
Ritik Kumar Pathak <phritik06@gmail.com>
2024-09-10 18:09:44 +0530

    middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,49 +1,28 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  console.log(new Date(), request.method, "=>", request.nextUrl.href);
   const path = request.nextUrl.pathname;
   const auth_path =
     path === "/login" || path === "/register" || path === "/verify-mail";
   const is_private_path =
-    path.startsWith("/api") || 
-    path.startsWith("/dashboard") || 
-    path.startsWith("/members") || 
-    path.startsWith("/leaves");
-  const token = request.cookies.get("token")?.value || "";
-  const is_bg_server = process.env.IS_BG_SERVER;
+    path.startsWith("/api") ||
+    path.startsWith("/dashboard") ||
+    path.startsWith("/members") ||
+    path.startsWith("/leave");
 
-  if (is_bg_server == "true") {
-    if (path.startsWith("/api")) {
-      return NextResponse.next();
-    } else {
-      return NextResponse.redirect(
-        new URL(
-          `https://qtee.ai${path}`,
-          request.nextUrl
-        ).toString()
-      );
-    }
-  } else {
-    if (auth_path && token) {
-      return NextResponse.redirect(
-        new URL("/dashboard", request.nextUrl).toString()
-      );
-    }
-    if (
-      is_private_path &&
-      !token &&
-      !path.startsWith("/api/")
-    ) {
-      return NextResponse.redirect(
-        new URL("/login", request.nextUrl).toString()
-      );
-    }
-    if (path === "/" && token) {
-      return NextResponse.redirect(
-        new URL("/dashboard", request.nextUrl).toString()
-      );
-    }
+  const token = request.cookies.get("token")?.value || "";
+  if (auth_path && token) {
+    return NextResponse.redirect(
+      new URL("/dashboard", request.nextUrl).toString()
+    );
+  }
+  if (is_private_path && !token && !path.startsWith("/api/")) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl).toString());
+  }
+  if (path === "/" && token) {
+    return NextResponse.redirect(
+      new URL("/dashboard", request.nextUrl).toString()
+    );
   }
 
   return NextResponse.next();
